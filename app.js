33d8b364e70ebe3956d68c3b45a11c7a3cca5707
Adam <acdoyle630@gmail.com>
2017-04-21 09:37:43 -1000

    sum of series, returns false if total sum is less than num

---
+++ app.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 6*/
+
+function seriesSum(arr, num){
+  if(arr.length === 0){
+    return false;
+  }
+  let sum = 0;
+  for(var i = 0; i < arr.length; i++){
+    sum += arr[i];
+    console.log(sum);
+    if(sum === num){
+      return true;
+    }
+  }
+  if(num > sum){
+    return false;
+  } else {
+  arr.shift();
+  return seriesSum(arr, num);
+  }
+}
+
+
+console.log(seriesSum([5,4,3,2,1],10));
